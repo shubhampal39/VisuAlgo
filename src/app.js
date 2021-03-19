d69c5fe12faac14f2a41c894fd07cb062e4c3da3
unknown <shubhampal39@gmail.com>
2021-03-19 07:20:59 +0530

    Added server files

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const path=require('path');
+const hbs=require('hbs')
 const Port=process.env.PORT ||3000;
-
 
 const app = express();
 const static_path = path.join(__dirname,'../public');
-console.log(static_path);
+const templates_path=(path.join(__dirname,'../template/views'));
+const partials_path=(path.join(__dirname,'../template/partials'));
+
+app.set("view engine","hbs");
+app.set('views',templates_path);
 app.use(express.static(static_path));
+hbs.registerPartials(partials_path);
 
 
-
-
-app.get("",(req, res) =>{
-    req.send("aaaaaaa");
+app.get("",(req,res) => {
+    res.render('index')
 })
+
 
 app.listen(Port,()=>{
     console.log(`Running server on Port no ${Port}`)
